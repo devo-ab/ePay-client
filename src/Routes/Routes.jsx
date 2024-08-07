c2e456c73dbfcb420e3b5e5b02682823d6a869a8
designer-ab <per.useravi0@gmail.com>
2024-08-07 20:38:17 +0600

    General Setup Complete

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Home/Home";
+import Error from "../Error/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
